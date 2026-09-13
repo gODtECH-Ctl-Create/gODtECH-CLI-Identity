851d8f83e93c7113de98c7b4bb0a014dfdf7e666
Ayo Richard ABE [gODtECH] <ayoabe24@gmail.com>
2026-09-13 11:01:38 +0100

    expose banner renderer

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { composeLogo, centerLine } from "./compose.js";
 export type { ComposeOptions } from "./compose.js";
+export { renderIdentity } from "./render.js";
+export type { RenderOptions } from "./render.js";
 export { getProduct, PRODUCTS } from "./products.js";
 export type { ProductIdentity } from "./products.js";
