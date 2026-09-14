a6da961a91c2cb2f82d1054e58f952bf214d6d19
Ayo Richard ABE [gODtECH] <ayoabe24@gmail.com>
2026-09-14 01:31:33 +0100

    chore: expose public identity types

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export { composeLogo, centerLine } from "./compose.js";
 export type { ComposeOptions } from "./compose.js";
 export { renderIdentity } from "./render.js";
 export type { RenderOptions } from "./render.js";
-export { getProduct, PRODUCTS } from "./products.js";
+export { PRODUCTS, getProduct } from "./products.js";
 export type { ProductIdentity } from "./products.js";
